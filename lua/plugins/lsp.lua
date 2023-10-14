a07ffbd311cdb0b603938311ad2d9cb472e97295
Hojin Lee <nyx1371@snu.ac.kr>
2023-10-14 18:13:34 +0900

    added treesitter & lsp

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        -- pyright will be automatically installed with mason and loaded with lspconfig
+        pyright = {},
+        clangd = {},
+      },
+    },
+  },
+}
---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      highlight = { enable = true },
+      ensure_installed = {
+        "bash",
+        "cpp",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+}
